580327d31e40c6881f7bd8e7bb61a05e87c428e9
sishi.gw <sishi.gw@alipay.com>
2018-04-11 17:41:28 +0800

    :rocket: custom appx

---
+++ src/config/appx.js
@@ -0,0 +1,8 @@
+export default {
+  appxCss: 'https://gw.alipayobjects.com/os/rmsportal/IwuRHIbaqzirgXIpgltn.css',
+  appxJs: 'https://gw.alipayobjects.com/os/rmsportal/jQwBpfGRZNIZPLXaCVfA.js',
+  appxWorkJs: 'https://gw.alipayobjects.com/os/rmsportal/MJOgDmBtEUMkFtqvffMh.js',
+  // appxCss: 'https://g-assets.daily.taobao.net/dingding/appx-sdk/0.1.1/af-appx.css',
+  // appxJs: 'https://g-assets.daily.taobao.net/dingding/appx-sdk/0.1.1/af-appx.ide.js',
+  // appxWorkJs: 'https://g-assets.daily.taobao.net/dingding/appx-sdk/0.1.1/af-appx.worker.ide.js',
+}
