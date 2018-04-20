eb0986df3cfc5963ca3cb3916bc4f7325a0bb35c
sishi.gw <sishi.gw@alipay.com>
2018-04-20 11:21:39 +0800

    update dingtalk appx version

--- src/config/appx.js
+++ src/config/appx.js
@@ -1,7 +1,7 @@
 export default {
-  appxCss: 'https://gw.alipayobjects.com/os/rmsportal/IwuRHIbaqzirgXIpgltn.css',
-  appxJs: 'https://gw.alipayobjects.com/os/rmsportal/jQwBpfGRZNIZPLXaCVfA.js',
-  appxWorkJs: 'https://gw.alipayobjects.com/os/rmsportal/MJOgDmBtEUMkFtqvffMh.js',
+  appxCss: 'https://gw.alipayobjects.com/os/rmsportal/ESYfAewtrqdggVTxfQRZ.css',
+  appxJs: 'https://gw.alipayobjects.com/os/rmsportal/ahOkOqHyOYuQteumYLJx.js',
+  appxWorkJs: 'https://gw.alipayobjects.com/os/rmsportal/gMvIQzUWpuOAEQnigBWd.js',
   // appxCss: 'https://g-assets.daily.taobao.net/dingding/appx-sdk/0.1.1/af-appx.css',
   // appxJs: 'https://g-assets.daily.taobao.net/dingding/appx-sdk/0.1.1/af-appx.ide.js',
   // appxWorkJs: 'https://g-assets.daily.taobao.net/dingding/appx-sdk/0.1.1/af-appx.worker.ide.js',
