38ae406f818b808fb8f3a134dba65758334342f5
sishi.gw <sishi.gw@alipay.com>
2018-04-20 11:22:05 +0800

    fix: devices in dingtalk

--- src/sidebar/simulator/devices.js
+++ src/sidebar/simulator/devices.js
@@ -1,36 +1,36 @@
 export default [
   {
-    device: 'dd iPhone 4',
+    device: 'iPhone 4',
     ratio: '320 X 568',
     dpr: 2.0,
     ua: 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E269 ChannelId(0) Nebula  AlipayDefined() AliApp(AP/10.1.0.082910) AlipayClient/10.1.0.082910 Language/en AlipayIDE/PuerTea AliApp(DingTalk/4.0.0)',
   },
   {
-    device: 'dd iPhone 6',
+    device: 'iPhone 6',
     ratio: '375 X 667',
     dpr: 2.0,
     ua: 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E269 ChannelId(0) Nebula  AlipayDefined() AliApp(AP/10.1.0.082910) AlipayClient/10.1.0.082910 Language/en AlipayIDE/PuerTea AliApp(DingTalk/4.0.0)',
   },
   {
-    device: 'dd iPhone 6 Plus',
+    device: 'iPhone 6 Plus',
     ratio: '414 X 736',
     dpr: 3.0,
     ua: 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E269 ChannelId(0) Nebula  AlipayDefined() AliApp(AP/10.1.0.082910) AlipayClient/10.1.0.082910 Language/en AlipayIDE/PuerTea AliApp(DingTalk/4.0.0)',
   },
   {
-    device: 'dd Galaxy S5',
+    device: 'Galaxy S5',
     ratio: '384 X 640',
     dpr: 3.0,
     ua: 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E269 ChannelId(0) Nebula  AlipayDefined() AliApp(AP/10.1.0.082910) AlipayClient/10.1.0.082910 Language/en AlipayIDE/PuerTea AliApp(DingTalk/4.0.0)',
   },
   {
-    device: 'dd Nexus 5x',
+    device: 'Nexus 5x',
     ratio: '411 X 731',
     dpr: 2.625,
     ua: 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E269 ChannelId(0) Nebula  AlipayDefined() AliApp(AP/10.1.0.082910) AlipayClient/10.1.0.082910 Language/en AlipayIDE/PuerTea AliApp(DingTalk/4.0.0)',
   },
   {
-    device: 'dd Nexus 6',
+    device: 'Nexus 6',
     ratio: '412 X 732',
     dpr: 3.5,
     ua: 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E269 ChannelId(0) Nebula  AlipayDefined() AliApp(AP/10.1.0.082910) AlipayClient/10.1.0.082910 Language/en AlipayIDE/PuerTea AliApp(DingTalk/4.0.0)',
