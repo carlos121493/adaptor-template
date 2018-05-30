d1f5bf11f17721e4c44277993396c42435db1edf
sishi.gw <sishi.gw@alipay.com>
2018-05-30 16:10:35 +0800

    update new framework

--- scripts/debugger.js
+++ scripts/debugger.js
@@ -5,8 +5,8 @@ const { join, dirname, basename } = require('path');
 const rootDir = join(__dirname, '..');
 const binPath = execSync('npm bin').toString().trim();
 const coreDir =  join(rootDir, '/node_modules/@alipay/ide-ts');
-const externalPath = dirname(require.resolve('@alipay/cygnus-ide-tiny-project'));
-const env = { ...process.env, ADAPTER_DEBUG: true, ADAPTER_DEBUG_PATH: rootDir }
+const externalPath = dirname(require.resolve('@alipay/volans-ide-tiny-project'));
+const env = { ...process.env, ADAPTOR_DEBUG: true, ADAPTOR_DEBUG_PATH: rootDir }
 
 spawn(join(binPath, 'electron'), [ require.resolve('@alipay/ide-ts'), `--externals=${externalPath}`], {
   cwd: coreDir,
--- src/simulator/appx.js
+++ src/simulator/appx.js
@@ -1,5 +1,5 @@
 export default {
-  appxCss: 'https://gw.alipayobjects.com/os/rmsportal/ESYfAewtrqdggVTxfQRZ.css',
-  appxJs: 'https://gw.alipayobjects.com/os/rmsportal/ahOkOqHyOYuQteumYLJx.js',
-  appxWorkJs: 'https://gw.alipayobjects.com/os/rmsportal/gMvIQzUWpuOAEQnigBWd.js',
+  appxCss: 'https://g-assets.daily.taobao.net/dingding/appx-sdk/1.7.4/af-appx.css',
+  appxJs: 'https://g-assets.daily.taobao.net/dingding/appx-sdk/1.7.4/af-appx.ide.js',
+  appxWorkJs: 'https://g-assets.daily.taobao.net/dingding/appx-sdk/1.7.4/af-appx.worker.ide.js',
 }
